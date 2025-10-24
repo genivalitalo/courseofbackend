ca6a9b20faee98b527dc68fec2b12bc3f02b3aef
genivalitalo <italobsilvaa@gmail.com>
2025-10-24 08:10:50 -0300

    feat: COnfigs of Eslint e Editor config

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,10 @@
 // Criar uma instância do servidor
-import express from 'express';
+import express from "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('Servidor está funcionando!');
-})
+server.get("/", (req, res) => {
+  res.send("Servidor está funcionando!");
+});
 
 export default server;
